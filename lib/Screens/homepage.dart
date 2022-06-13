b44f540dddc9896c1d2b47b527a4e106b9759534
MrKamal614 <kamal.k@softserv.in>
2022-06-14 00:49:25 +0530

    Fetching in progress

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_registration_app/Screens/register_page.dart';
 import 'package:firebase_registration_app/Widgets/common_widgets.dart';
 import 'package:firebase_registration_app/constants/string_constants.dart';
@@ -12,25 +13,40 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  var fireStoreData =
+      FirebaseFirestore.instance.collection("regUserInfo").snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        title: Text(GlobalConstants().homepage),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            commonButton(context, GlobalConstants().logout, () {
-              Get.offAll(const RegisterApp());
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text(GlobalConstants().homepage),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: fireStoreData,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) 
+               {
+                 return const Center(child: CircularProgressIndicator());
+               }
+                return ListView.builder(itemCount: snapshot.data!.docs.length ,itemBuilder: (context, int index {
+                  
+                }));
+              
             })
-          ],
-        )),
-      ),
-    );
+
+        // Padding(
+        //   padding: const EdgeInsets.all(8.0),
+        //   child: Center(
+        //       child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       commonButton(context, GlobalConstants().logout, () {
+        //         Get.offAll(const RegisterApp());
+        //       })
+        //     ],
+        //   )),
+        // ),
+        );
   }
 }
